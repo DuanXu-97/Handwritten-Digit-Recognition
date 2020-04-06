5d92d8b75f29fabfe58810a300206e2e965370a0
DuanXu-97 <956378834@qq.com>
2020-04-06 17:59:46 +0800

    update

--- models/train.py
+++ models/train.py
@@ -41,7 +41,7 @@ def train(args, config):
 
     best_valid_loss = 0
     best_epoch = 0
-    dist_to_best = 4
+    dist_to_best = 0
 
     time_begin = time.clock()
 
@@ -90,7 +90,9 @@ def train(args, config):
 
         valid_cm = valid_confusion_matrix.value()
         valid_accuracy = 100. * (valid_cm.diagonal().sum()) / (valid_cm.sum())
+
         vis.plot('valid_accuracy', valid_accuracy)
+        print('valid_accuracy', valid_accuracy)
 
         vis.log("epoch:{epoch}, lr:{lr}, train_loss:{train_loss}, train_cm:{train_cm}, valid_loss:{valid_loss}, valid_cm:{valid_cm}".format(
             epoch=epoch,
@@ -100,12 +102,10 @@ def train(args, config):
             valid_cm=str(valid_cm),
             lr=config.lr
         ))
-        print("epoch:{epoch}, lr:{lr}, train_loss:{train_loss}, train_cm:{train_cm}, valid_loss:{valid_loss}, valid_cm:{valid_cm}".format(
+        print("epoch:{epoch}, lr:{lr}, train_loss:{train_loss}, valid_loss:{valid_loss}".format(
             epoch=epoch,
             train_loss=train_loss_meter.value()[0],
-            train_cm=str(train_confusion_matrix.value()),
             valid_loss=valid_loss_meter.value()[0],
-            valid_cm=str(valid_cm),
             lr=config.lr
         ))
 
