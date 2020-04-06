9d0d8c746c2af509709bf54e091f6a83fe744b8d
DuanXu-97 <956378834@qq.com>
2020-04-06 17:18:57 +0800

    update

--- models/train.py
+++ models/train.py
@@ -129,6 +129,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     config = configs.DefaultConfig()
 
+    if os.path.exists(args.ckpts_dir):
+        os.makedirs(args.ckpts_dir)
+
     train(args, config)
 
 
